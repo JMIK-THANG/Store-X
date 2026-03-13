9d4cab8b89f76fa4aaad3dfb42679f7b6a13d588
Jmik Thang <jmikthang5@gmail.com>
2026-03-13 08:04:56 -0400

    dropdown menu added

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 
 const ProductPage = () => {
-  const [prodcucts, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState("All");
   const [uniqueCategories, setUniqueCategories] = useState("All");
   const [error, setError] = useState("");
 
   async function getProducts() {
     try {
-      let fetchData = await fetch("https://fakestoreapi.com/products");
-      let productsData = await fetchData.json();
+      let response = await fetch("https://fakestoreapi.com/products");
+      let productsData = await response.json();
+      console.log(productsData)
 
       let uniqueCategories = Array.from(
         new Set(productsData.map((product) => product.category)),
@@ -27,9 +29,14 @@ const ProductPage = () => {
     <>
       <h1>Store-X</h1>
       <label>Choose By Category</label>
-      <select>
-        <option value="All">All</option>
-        {}
+      <select
+        value={selectedCategories}
+        onChange={(e) => setSelectedCategories(e.target.value)}
+      >
+        ;<option value="All">All</option>
+        {products.map((product) => (
+          <option value={product}>{product}</option>
+        ))}
       </select>
     </>
   );

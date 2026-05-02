37a2613e2148d2867702cf463af4266a3d8198f8
Jmik Thang <jmikthang5@gmail.com>
2026-05-01 21:33:20 -0400

    create env file

--- server/config/db.js
+++ server/config/db.js
@@ -5,14 +5,11 @@ const { Client } = pg;
 
 const db = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 db.connect()
   .then(() => console.log("Database is connected"))
   .catch((err) => console.error("Connection error:", err));
-
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 export default db;
